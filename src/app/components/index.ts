59af1977f4ad8e57e49e9a492005991e6f5dc863
Jonayet <jonayet.bu@gmail.com>
2017-01-14 03:07:34 +0600

    menu component restored

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,11 +1,14 @@
+import {MenuComponent} from './menu/menu.component';
 import {MenuService} from './menu/menu.service';
 import {GridComponent} from './grid/grid.component';
 
 export const Components = {
+    MenuComponent,
     GridComponent
 };
 
 export const ComponentRegistry = [
+    MenuComponent,
     GridComponent
 ];
 
